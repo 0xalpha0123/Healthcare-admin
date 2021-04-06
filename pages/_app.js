1f1022616bfdbc53a2f0596db89789cdf0cbf1e8
wayne-th <wayned.eth@gmail.com>
2021-04-06 22:06:33 +0200

    setup app with docker and i18n

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
