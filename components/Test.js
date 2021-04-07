ed597e3107896fc8c67b9c3f5b05b719dffec064
lahm0123 <lahm0123@outlook.com>
2021-04-07 21:00:48 +0200

    MaterialUI added

--- components/Test.js
+++ components/Test.js
@@ -1,8 +1,13 @@
 import { useTranslation } from "next-i18next";
+import Button from "@material-ui/core/Button";
 
 function Test() {
   const { t } = useTranslation("common");
-  return <div>{t("save")}</div>;
+  return (
+    <Button variant="contained" color="primary">
+      {t("save")}
+    </Button>
+  );
 }
 
 export default Test;
