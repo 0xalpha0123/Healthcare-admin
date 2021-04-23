e6728e61ab304bbe99b0d6b200d951fe704646c2
lahm0123 <lahm0123@outlook.com>
2021-04-24 01:44:25 +0200

    refactor

--- components/ui/Sidebar/MenuItem/index.js
+++ components/ui/Sidebar/MenuItem/index.js
@@ -1,15 +1,15 @@
 import { Box, Card, Container, Typography } from "@material-ui/core";
 
-const MenuItem = ({ item, url, label, image, activeUrl }) => (
-  <Box item className="MenuItem__buttonWrapper">
+const MenuItem = ({ currentUrl, image, label, url }) => (
+  <Box className="MenuItem__buttonWrapper">
     <a href={url}>
       <Card
         className={`MenuItem__button ${
-          activeUrl === url ? "MenuItem__button_active" : ""
+          currentUrl === url ? "MenuItem__button_active" : ""
         }`}
       >
         <Container>
-          <Box display="flex" justify-content="center" my={1}>
+          <Box my={1} display="flex" justify-content="center">
             <img alt={label} src={image} className="MenuItem__image" />
           </Box>
           <Box my={1}>
